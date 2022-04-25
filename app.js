0ab88ea1f87d6df17bbfa66182b070cf82920b1c
Sourav Saraf <souravsaraf2000@rediffmail.com>
2022-04-25 15:15:20 +0530

    Admin Panel Data

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const adminDataRoute = require('./route')
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use('/',adminDataRoute)
 
 app.listen(process.env.PORT || 5000)
